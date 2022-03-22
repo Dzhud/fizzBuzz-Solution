dd328e8baa4168ad9d2d4ab90746b4016f887e5a
Adeleke Omotayo <omotayo8@live.com>
2022-03-22 20:57:45 +0100

    Added another solution to the challenge

--- fizzy.py
+++ fizzy.py
@@ -1,4 +1,8 @@
+# Multiples of 3 and 5 return 'Fizz' and 'Buzz' respectively while 15 returns 'FizzBuzz'
+
 def fizzBuzz():
+	# Solution 1
+	'''
 	for i in range(1, 16):
 		 if i in [3, 6, 9, 12]:
 			 i='Fizz'
@@ -7,5 +11,16 @@ def fizzBuzz():
 		 if i==15:
 			 i='FizzBuzz'
 		 print(i)
+	'''
+	# Solution 2
+	for ii in range(1, 16):
+                if ii == 15:
+                        ii='FizzBuzz'
+                elif ii % 3 == 0:
+                        ii='Fizz'
+                elif ii % 5 == 0:
+                        ii='Buzz'
+                print(ii)
 
-# Multiples of 3 and 5 return 'Fizz' and 'Buzz' respectively while 15 returns 'FizzBuzz'
+if __name__ == "__main__":
+	fizzBuzz()
